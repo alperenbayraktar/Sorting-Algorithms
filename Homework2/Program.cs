58bfd49ce33b66e80719fab128a2b28ee7efd9ee
Alperen Bayraktar <alperenbayraktar96@gmail.com>
2019-04-27 17:06:02 +0300

    Insertion Sort

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -10,7 +10,37 @@ namespace Homework2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            int[] input = { 12, 11, 13, 5, 6 };
+            InsertionSort(input);
+            printArray(input);
+        }
+        static void InsertionSort(int[] input)
+        {
+            int n = input.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = input[i];
+                int j = i - 1;
+
+                // Move elements of arr[0..i-1], 
+                // that are greater than key, 
+                // to one position ahead of 
+                // their current position 
+                while (j >= 0 && input[j] > key)
+                {
+                    input[j + 1] = input[j];
+                    j = j - 1;
+                }
+                input[j + 1] = key;
+            }
+        }
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+
+            Console.Write("\n");
         }
     }
 }
