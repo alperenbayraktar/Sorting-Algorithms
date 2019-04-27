6bb479d538747489e47285d4fc07deaa5ea1988d
Alperen Bayraktar <alperenbayraktar96@gmail.com>
2019-04-27 19:00:29 +0300

    Added HeapSort, QuickSort from Emre

--- Homework2 Alperen/Program.cs
+++ Homework2 Alperen/Program.cs
@@ -15,16 +15,36 @@ namespace Homework2
             InsertionSort ins = new InsertionSort();
             ins.sort(inputInsertion);
             printArray(inputInsertion);
+
             int[] inputMerge = { 76, 89, 23, 1, 55, 78, 99, 12, 65, 100 };
             Console.WriteLine("MergeSort: ");
             MergeSort mer = new MergeSort();
             mer.mergeSort(inputMerge, 0, inputMerge.Length - 1);
             printArray(inputMerge);
+
             int[] inputCount = { 762, 819, 235, 12, 545, 178, 99, 12, 645, 100 };
             Console.WriteLine("CountingSort: ");
             CountingSort con = new CountingSort();
             con.sort(inputCount);
             printArray(inputCount);
+
+            int[] inputQuick = { 67, 12, 95, 56, 85, 1, 100, 23, 60, 9 };
+            Console.WriteLine("Quick Sort: ");
+            Console.Write("Initial array is: ");
+            printArray(inputQuick);
+            QuickSort quick = new QuickSort();
+            quick.quickSort(inputQuick, 0, inputQuick.Length - 1);
+            Console.Write("Sorted Array is: ");
+            printArray(inputQuick);
+
+            int[] inputHeap = { 55, 25, 89, 34, 12, 19, 78, 95, 1, 100 };
+            Console.WriteLine("Heap Sort: ");
+            Console.Write("Initial array is: ");
+            printArray(inputHeap);
+            HeapSort heap = new HeapSort();
+            heap.heapSort(inputHeap, 10);
+            Console.Write("Sorted Array is: ");
+            printArray(inputHeap);
         }
         static void printArray(int[] arr)
         {
