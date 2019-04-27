a81c187e3a5f4f23286b968364d796599dba9819
Alperen Bayraktar <alperenbayraktar96@gmail.com>
2019-04-27 17:35:08 +0300

    Added MergeSort

--- Homework2 Alperen/Program.cs
+++ Homework2 Alperen/Program.cs
@@ -11,8 +11,13 @@ namespace Homework2
         static void Main(string[] args)
         {
             int[] input = { 12, 11, 13, 5, 6 };
+            Console.WriteLine("InsertionSort: ");
             InsertionSort(input);
             printArray(input);
+            int[] arr = { 76, 89, 23, 1, 55, 78, 99, 12, 65, 100 };
+            Console.WriteLine("MergeSort: ");
+            mergeSort(arr, 0, input.Length - 1);
+            printArray(arr);
         }
         static void InsertionSort(int[] input)
         {
@@ -32,6 +37,61 @@ namespace Homework2
                     j = j - 1;
                 }
                 input[j + 1] = key;
+            }
+        }
+        static public void merge(int[] arr, int p, int q, int r)
+        {
+            int i, j, k;
+            int n1 = q - p + 1;
+            int n2 = r - q;
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            for (i = 0; i < n1; i++)
+            {
+                L[i] = arr[p + i];
+            }
+            for (j = 0; j < n2; j++)
+            {
+                R[j] = arr[q + 1 + j];
+            }
+            i = 0;
+            j = 0;
+            k = p;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+        static public void mergeSort(int[] arr, int p, int r)
+        {
+            if (p < r)
+            {
+                int q = (p + r) / 2;
+                mergeSort(arr, p, q);
+                mergeSort(arr, q + 1, r);
+                merge(arr, p, q, r);
             }
         }
         static void printArray(int[] arr)
