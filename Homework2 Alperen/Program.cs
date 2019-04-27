6586548effa8608052a70f41d56ab95471305d25
Alperen Bayraktar <alperenbayraktar96@gmail.com>
2019-04-27 20:15:10 +0300

    Added String Resources, More Clear Code

--- Homework2 Alperen/Program.cs
+++ Homework2 Alperen/Program.cs
@@ -3,48 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Homework2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] inputInsertion = { 12, 11, 13, 5, 6 };
-            Console.WriteLine("InsertionSort: ");
-            InsertionSort ins = new InsertionSort();
-            ins.sort(inputInsertion);
-            printArray(inputInsertion);
+            int[] input = { 76, 89, 23, 1, 55, 78, 99, 12, 65, 100 };
 
-            int[] inputMerge = { 76, 89, 23, 1, 55, 78, 99, 12, 65, 100 };
-            Console.WriteLine("MergeSort: ");
-            MergeSort mer = new MergeSort();
-            mer.mergeSort(inputMerge, 0, inputMerge.Length - 1);
-            printArray(inputMerge);
-
-            int[] inputCount = { 762, 819, 235, 12, 545, 178, 99, 12, 645, 100 };
-            Console.WriteLine("CountingSort: ");
-            CountingSort con = new CountingSort();
-            con.sort(inputCount);
-            printArray(inputCount);
-
-            int[] inputQuick = { 67, 12, 95, 56, 85, 1, 100, 23, 60, 9 };
-            Console.WriteLine("Quick Sort: ");
-            Console.Write("Initial array is: ");
-            printArray(inputQuick);
-            QuickSort quick = new QuickSort();
-            quick.quickSort(inputQuick, 0, inputQuick.Length - 1);
-            Console.Write("Sorted Array is: ");
-            printArray(inputQuick);
-
-            int[] inputHeap = { 55, 25, 89, 34, 12, 19, 78, 95, 1, 100 };
-            Console.WriteLine("Heap Sort: ");
-            Console.Write("Initial array is: ");
-            printArray(inputHeap);
-            HeapSort heap = new HeapSort();
-            heap.heapSort(inputHeap, 10);
-            Console.Write("Sorted Array is: ");
-            printArray(inputHeap);
+            execute("insertion", input);
+            execute("merge", input);
+            execute("counting", input);
+            execute("quick", input);
+            execute("heap", input);
         }
         static void printArray(int[] arr)
         {
@@ -53,6 +24,54 @@ namespace Homework2
                 Console.Write(arr[i] + " ");
 
             Console.Write("\n");
+        }
+        static void execute(string selection, int[] input)
+        {
+            int[] output = new int[input.Length];
+            Array.Copy(input, output, input.Length);
+            StringResources sr = new StringResources();
+            string title = "";
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            switch (selection)
+            {
+                case "insertion":
+                    title = sr.insertion;
+                    InsertionSort ins = new InsertionSort();
+                    ins.sort(output);
+                    break;
+                case "merge":
+                    title = sr.merge;
+                    MergeSort mer = new MergeSort();
+                    mer.mergeSort(output, 0, output.Length - 1);
+                    break;
+                case "counting":
+                    title = sr.counting;
+                    CountingSort con = new CountingSort();
+                    con.sort(output);
+                    break;
+                case "quick":
+                    title = sr.quick;
+                    QuickSort quick = new QuickSort();
+                    quick.quickSort(output, 0, output.Length - 1);
+                    break;
+                case "heap":
+                    title = sr.heap;
+                    HeapSort heap = new HeapSort();
+                    heap.heapSort(output, 10);
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine(title);
+            Console.WriteLine(sr.init);
+            printArray(input);
+            Console.WriteLine(sr.sorted);
+            printArray(output);
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.Write(sr.time);
+            Console.WriteLine(elapsedMs);
+            Console.WriteLine();
         }
     }
 }
---
+++ Homework2 Alperen/StringResources.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class StringResources
+    {
+        public String sorted = "Sorted Array: ";
+        public String insertion = "InsertionSort: ";
+        public String heap = "HeapSort: ";
+        public String quick = "QuickSort: ";
+        public String counting = "CountingSort: ";
+        public String merge = "MergeSort: ";
+        public String init = "Initial array: ";
+        public String time = "Execution time: ";
+    }
+}
