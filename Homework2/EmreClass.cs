ad3ac271e4328fac71ccf36b1100197801cea049
Emre Erdem <eerdem95@hotmail.com>
2019-04-27 16:29:55 +0300

    Create EmreClass.cs

---
+++ Homework2/EmreClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class EmreClass
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("EmreLO\n");
+        }
+    }
+}
